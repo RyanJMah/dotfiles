0bb2c07c2fd8c130b55a8ed6fcaa1f3507792e1e
Ryan Mah <rmah@uwaterloo.ca>
2021-10-28 17:14:51 -0600

    initial commit

---
+++ install.sh
@@ -0,0 +1,56 @@
+THIS_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo "starting install..."
+
+echo "\ninstalling oh-my-zsh..."
+if [ -d $HOME/.oh-my-zsh ]; then
+    echo "already installed, skipping..."
+else
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    source $HOME/.zshrc
+fi
+
+echo "\ninstalling syntax highlighting plugin..."
+if [ -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    echo "already installed, skipping..."
+else
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+echo "\ncopying oh-my-zsh dotfiles..."
+cp $THIS_DIR/.zshrc $HOME
+cp $THIS_DIR/.zsh_custom $HOME
+cp $THIS_DIR/ryan-theme.zsh-theme $HOME/.oh-my-zsh/themes
+source $HOME/.zsh_custom
+
+echo "\ninstalling nvim..."
+if command -v nvim > /dev/null; then
+    echo "already installed, skipping..."
+else
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install neovim
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+        chmod u+x nvim.appimage
+        mv nvim.appimage /usr/bin
+    fi
+fi
+
+echo "\ninstalling vim-plug..."
+if [ -f $HOME/.vim/autoload/plug.vim ]; then
+    echo "already installed, skipping..."
+else
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+echo "\ncopying vim dotfiles..."
+cp $THIS_DIR/.vimrc $HOME
+mkdir -p $HOME/.config/nvim
+cp $THIS_DIR/init.vim $HOME/.config/nvim
+
+echo "\ninstalling vim plugins..."
+vim +PluginInstall +qall
+
+echo ""
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# My Dotfiles
+
+```shell
+git clone https://github.com/RyanJMah/dotfiles.git
+cd dotfiles
+./install.sh
+```
+
