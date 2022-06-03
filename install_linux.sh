0520d0a0dbd072209a25db4834c8288054507e51
Ryan Mah <rmah@uwaterloo.ca>
2022-06-02 22:59:50 -0400

    updated nvim config and nvim version

--- install_linux.sh
+++ install_linux.sh
@@ -12,7 +12,7 @@ then
     sudo apt install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
     git clone https://github.com/neovim/neovim.git
     cd neovim
-    git checkout release-0.6
+    git checkout release-0.7
     make
     sudo make install
     cd ..
